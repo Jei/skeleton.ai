0ce759dd254106ace04be48b801aace62e6abbd4
Andrea Jonus <jei@carrera>
2017-03-12 00:24:32 +0100

    Finalized IRC IO module.

--- io/irc.js
+++ io/irc.js
@@ -5,20 +5,34 @@ const IRCBot = require('irc');
 class IRC extends require('./iodriver') {
 	constructor() {
 		super();
-		this.bot = new IRC(_config.serverUrl, _config.nick, _config.options);
+		this.bot = new irc.Client(_config.serverUrl, _config.nick, _config.options);
 
-		// TODO ?
-
-		this.bot.on('message', (e) => {
-			this.emit('message', e);
+		this.bot.addListener('message', (from, to, message) => {
+			this.emit('message', from, to, message);
 		});
 	}
 
 	output(data, e) {
 		return new Promise((resolve, reject) => {
 			console.ai(TAG, e);
+			if (_.isString(e)) e = { text: e };
 
-			// TODO message send logic
+			if (e.error) return resolve();
+
+			if (e.text) {
+				this.bot.say(data.target, e.text);
+				return resolve();
+			}
+
+			if (e.spotify) {
+				if (e.spotify.song) {
+					this.bot.say(data.target, e.spotify.song.external_urls.spotify);
+					return resolve();
+				}
+				return reject();
+			}
+
+			return reject();
 		});
 	}
 }
