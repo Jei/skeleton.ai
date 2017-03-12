e9886cb3f8f07b73d340b8dacfaadb931edd0452
Jei <j.jonus.j@gmail.com>
2017-03-12 02:41:26 +0100

    Fixed wrong config require.

--- io/irc.js
+++ io/irc.js
@@ -1,14 +1,30 @@
 const TAG = 'IO.IRC';
-const _config = require('./config.json').telegram || {};
+const _config = require('./config.json').irc || {};
 const IRCBot = require('irc');
 
 class IRC extends require('./iodriver') {
 	constructor() {
 		super();
-		this.bot = new irc.Client(_config.serverUrl, _config.nick, _config.options);
+		this.bot = new IRCBot.Client(_config.serverUrl, _config.nick, _config.options);
 
 		this.bot.addListener('message', (from, to, message) => {
 			this.emit('message', from, to, message);
+		});
+
+		this.bot.addListener('registered', (message) => {
+			console.io(TAG, 'registered to the server ' + message.server);
+		});
+
+		this.bot.addListener('join', (channel, nick, message) => {
+			console.io(TAG, nick + ' joined ' + channel);
+		});
+
+		this.bot.addListener('part', (channel, nick, reason, message) => {
+			console.io(TAG, nick + ' parted ' + channel + ' (reason: ' + reason + ')');
+		});
+
+		this.bot.addListener('error', (e) => {
+			console.error(TAG, e);
 		});
 	}
 
--- main.js
+++ main.js
@@ -124,8 +124,10 @@ function onIoResponse(err, data, para) {
 config.ioDrivers.forEach((driver_name) => {
 	let Driver = require(__basedir + '/io/' + driver_name);
 	IOs[driver_name] = new Driver();
+	console.info('Created IO driver "' + driver_name + '"');
 });
 
 config.behaviors.forEach((behavior_name) => {
 	Behaviors[behavior_name] = require(__basedir + '/behaviors/' + behavior_name);
+	console.info('Added behavior "' + behavior_name + '"');
 });
