75082359379ed0e28c15ca14b8e3388f111af335
Jei <j.jonus.j@gmail.com>
2017-03-16 17:26:14 +0100

    Fixed requires.

--- main.js
+++ main.js
@@ -121,14 +121,14 @@ function onIoResponse(err, data, para) {
 	}
 }
 
-_.each(config.ioDrivers, (driver_name) => {
+_.each(config.ioDrivers, (opts, driver_name) => {
 	let Driver = require(__basedir + '/io/' + driver_name);
-	IOs[driver_name] = new Driver(config.ioDrivers[driver_name]);
+	IOs[driver_name] = new Driver(opts);
 	console.info('Created IO driver "' + driver_name + '"');
 });
 
-_.each(config.behaviors, (behavior_name) => {
-	// TODO refactor as classes?
+_.each(config.behaviors, (opts, behavior_name) => {
+	// TODO find the right way to pass parameters to the behaviors
 	Behaviors[behavior_name] = require(__basedir + '/behaviors/' + behavior_name);
 	console.info('Added behavior "' + behavior_name + '"');
 });
