38f95ee69ed62b64bb84afb3675740d660f692dd
Jei <j.jonus.j@gmail.com>
2017-03-16 18:44:43 +0100

    Small console dump.

--- behaviors/telegramChatBot.js
+++ behaviors/telegramChatBot.js
@@ -1,13 +1,20 @@
-const telegram_config = require('../io/config.json').telegram || {};
-const _config = require('./config.json') || {};
+const telegram_config = config.ioDrivers.telegram || {};
+const _config = config.behaviors.telegramChatBot || {};
 
-if (IOs.telegram && IOs.irc && _config.telegramChatBot) {
-	const bot_config = _config.telegramChatBot;
-
+if (IOs.telegram && AIs.apiai && _config) {
 	IOs.telegram.on('message', (e) => {
 		console.info('Message from Telegram', e);
 
 		// TODO support different types of messages
 		// TODO check whitelist/blacklist
+		if (e.text) {
+			AIs.apiai.textRequest(null, e.text)
+			.then((result) => {
+				console.info(result);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+		}
 	});
 }
