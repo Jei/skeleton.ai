b28f7353ec5a8004c009f55581c63d6dadbfebe0
Jei <j.jonus.j@gmail.com>
2017-03-24 12:31:29 +0100

    Finalized MyAnimeList.list() and MyAnimeList.myList()

--- handlers/myanimelist.js
+++ handlers/myanimelist.js
@@ -1,6 +1,9 @@
 const TAG = require('path').basename(__filename, '.js');
 const MALAPPINFO = 'https://myanimelist.net/malappinfo.php';
 
+// TODO create support module for localization
+const i18n = require('i18n-nodejs')(config.language || 'en', require('fs').existsSync('../i18n/' + TAG + '.json') ? '../i18n/' + TAG + '.json' : '../i18n/default.json');
+
 class MyAnimeList {
 	constructor(cfg) {
 		this._config = cfg;
@@ -18,21 +21,42 @@ class MyAnimeList {
 
 	list(result, data) {
 		return new Promise((resolve, reject) => {
-			_.defaults(data, {
+			let query = _.extend({
 				u: this._config.defaultUser || ''
-			});
+			}, result.parameters);
 
-			this.malappinfo(data, (err, resp) => {
+			this.malappinfo(query, (err, resp) => {
 				require('xml2js').parseString(resp, function(err, result) {
-					console.log(result);
-					// TODO handle empty list
+					let malresult = result.myanimelist;
 
-					if (result.myanimelist == null) return reject();
+					if (malresult == null) return reject();
 
-					return reject();
+					if (malresult.myinfo == null) {
+						return resolve({
+							text: i18n.__("Sorry, I couldn't find any data for the user {{u}}", query)
+						});
+					}
+
+					if (malresult.anime == null || malresult.manga == null) {
+						return resolve({
+							text: i18n.__("The user {{u}} has no {{type}} in his list yet.", query)
+						});
+					}
+
+					return resolve({
+						text: _.map(malresult[query.type || 'anime'], function(entry) {
+							return entry.series_title + '\n';
+						})
+					});
 				});
 			});
 		});
+	}
+
+	myList(result, data) {
+		return this.list(result, _.extend(data, {
+			u: this._config.defaultUser || ''
+		}));
 	}
 }
 
