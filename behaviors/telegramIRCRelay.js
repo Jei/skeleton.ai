6d43f6ab3b5c24b923f4d84638bdf6585af75782
Jei <j.jonus.j@gmail.com>
2017-08-09 23:49:54 +0200

    Fixes to the telegramIRCRelay behavior.

--- behaviors/telegramIRCRelay.js
+++ behaviors/telegramIRCRelay.js
@@ -2,6 +2,44 @@ const telegram_config = config.ioDrivers.telegram || {};
 const irc_config = config.ioDrivers.irc || {};
 const _config = config.behaviors.telegramIRCRelay || {};
 
+function performCommand(text, entity) {
+	let command_text = text.substr(entity.offset+1, entity.length-1);
+	let match = command_text.match(/^([a-z]+)[A-Z]?/);
+
+	if (match != null && match[1] != null) {
+		let handler_name = match[1];
+		let method = command_text.substring(handler_name.length);
+		if (method.length > 0) {
+			method = method.substring(0,1).toLowerCase() + method.substring(1);
+		}
+		let params = text.substring(entity.offset + entity.length);
+		let next_cmd_pos = params.indexOf('/');
+		if (next_cmd_pos >= 0) {
+			params = params.substring(0, next_cmd_pos);
+		}
+		params = _.compact(params.split(' '));
+
+		if (!_.isEmpty(method)) {
+
+			if (!_.isFunction(Handlers[handler_name])) {
+				return Promise.reject('Handler not found: ' + handler_name);
+			}
+
+			let Handler = Handlers[handler_name]();
+
+			return Handler[method].apply(Handler, params);
+		} else if (_.isFunction(Handlers[command_text])) {
+			// TODO check if action is class?
+			return Handlers[command_text](params);
+		} else {
+			return Promise.reject('Method not found: ' + command_text);
+		}
+	} else {
+		return Promise.reject('Could not parse entity: ' + command_text);
+	}
+}
+
+
 if (IOs.telegram && IOs.irc && _config) {
     IOs.telegram.on('message', (e) => {
         console.info('Message from Telegram', e);
@@ -32,15 +70,10 @@ if (IOs.telegram && IOs.irc && _config) {
             return;
         }
 
-        if (/^\!([a-z0-9]+) /.match(message)[1] === "telegram") {
-            performCommand(e.text, entity)
-            .then((result) => {
-                IOs.telegram.output(e.chat.id, result);
-            })
-            .catch((err) => {
-                // TODO send error message?
-                console.error(err);
-            });
+		const command = /^(\![a-z0-9]+) /.exec(message);
+
+        if (command && command[1] === "!telegram") {
+			IOs.telegram.output(_config.telegramChatId, 'IRC.' + from + ': ' + message.replace('!telegram ', ''));
         }
     });
 }
